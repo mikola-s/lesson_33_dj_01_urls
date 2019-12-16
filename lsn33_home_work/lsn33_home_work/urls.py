8972699027004465ca2cc2e1351263d995d34967
mikola-s <suharev.nv@gmail.com>
2019-12-17 01:21:39 +0200

    lsn33 add path in main urls.py

--- lsn33_home_work/lsn33_home_work/urls.py
+++ lsn33_home_work/lsn33_home_work/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('level_01/', include("level_one.urls")),
+    path('level_02/', include("level_two.urls")),
+    path('level_03/', include("level_three.urls")),
 ]
