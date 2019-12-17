69d6cd92540622157f60c306436e1af4c008b552
mikola-s <suharev.nv@gmail.com>
2019-12-17 12:01:56 +0200

    lsn33/comments/ create template in urls and views

--- lsn33_home_work/comments/urls.py
+++ lsn33_home_work/comments/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
-# from .views import first
+from django.urls import path, re_path
+from . import views
 
 urlpatterns = [
+    path('', views.base),
+    re_path('', views.nnsss),
+    re_path('', views.n_s_n),
+    re_path('', views.phone),
 ]
--- lsn33_home_work/comments/views.py
+++ lsn33_home_work/comments/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def base(request):
+    return HttpResponse('')
+
+
+def nnsss(request):
+    return HttpResponse('')
+
+
+def n_s_n(request):
+    return HttpResponse('')
+
+
+def phone(request):
+    return HttpResponse('')
