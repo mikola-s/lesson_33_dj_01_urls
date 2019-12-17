13f77b06e712b57a17874bc395a3649d6943b90c
mikola-s <suharev.nv@gmail.com>
2019-12-17 12:03:48 +0200

    lsn33/comments/urls write grep in path

--- lsn33_home_work/comments/urls.py
+++ lsn33_home_work/comments/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.base),
-    re_path('', views.nnsss),
-    re_path('', views.n_s_n),
-    re_path('', views.phone),
+    re_path(r'^\d{2}[A-z]{3}$', views.nnsss),
+    re_path(r'^\d-[A-z]-\d', views.n_s_n),
+    re_path(r'(050|066|095|099|039|067|068|096|097|098|063|093|091|092|094)-\d{3}(-\d\d){2}', views.phone),
 ]
+
+
