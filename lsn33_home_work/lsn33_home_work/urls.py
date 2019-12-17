9f8b60d4cae7e7c39d9ca2b3e26c551edf32ca9d
mikola-s <suharev.nv@gmail.com>
2019-12-17 07:29:26 +0200

    lsn33 add empty urlpatterns

--- lsn33_home_work/lsn33_home_work/urls.py
+++ lsn33_home_work/lsn33_home_work/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('level_01/', include("level_one.urls")),
