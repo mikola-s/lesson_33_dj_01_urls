48014eb5635313d82460b2a99699e48385b6b27d
mikola-s <suharev.nv@gmail.com>
2019-12-17 10:48:51 +0200

    lsn33/article/views add mock funktion

--- lsn33_home_work/article/views.py
+++ lsn33_home_work/article/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def base(request, id):
+    return HttpResponse('')
+
+
+def add(request, id):
+    return HttpResponse('')
+
+
+def delete(request, id):
+    return HttpResponse('')
