e26dc78096172a1217f8dcaec62d72cdfab51a11
mikola-s <suharev.nv@gmail.com>
2019-12-17 10:28:53 +0200

    lsn33 add views function in path in news/urls.py

--- lsn33_home_work/news/urls.py
+++ lsn33_home_work/news/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-# from .views import first
+from . import views
 
 urlpatterns = [
+    path('', views.base, name='news_base'),
+    path('new/', views.new, name='news_new'),
+    path('edit/', views.edit, name='news_edit'),
+    path('lock/', views.lock, name='news_lock'),
 ]
