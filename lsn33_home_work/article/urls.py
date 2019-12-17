2c16a101f3419e66736cf405fc8d26a2e02e54ae
mikola-s <suharev.nv@gmail.com>
2019-12-17 10:47:46 +0200

    lsn33/article add path in urlpatterns

--- lsn33_home_work/article/urls.py
+++ lsn33_home_work/article/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-# from .views import first
+from . import views
 
 urlpatterns = [
+    path('<int:id>/', views.base),
+    path('<int:id>/add', views.add),
+    path('<int:id>/delete', views.delete),
 ]
